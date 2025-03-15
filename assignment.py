27cc7839f3c2e48c5d544c12add559502f2f9ffd
ellah-ui <stlangulu@gmail.com>
2025-03-15 19:50:26 +0300

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -6,3 +6,11 @@ print(Score1 + Score2 + Score3)
 print((Score1 + Score2 + Score3) / 3)
 print(Score1 - Score2 - Score3)
 print(Score1 * Score2 * Score3)
+
+
+Results
+[Running] python -u "/Users/stellahangulu/git-test/assignment.py"
+600
+200.0
+-400
+6000000
